486ba858e6c3e585f9b6c7e9ad2dfb829303a607
Fernand084 <edgar.lopezr84@outlook.com>
2025-08-02 10:47:33 -0600

    Add configuration package for jackson and CORS and  properties for alloedOrigins to app.properties.

---
+++ src/main/java/com/fernando84/employeeapi/config/JacksonConfig.java
@@ -0,0 +1,22 @@
+package com.fernando84.employeeapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new ParameterNamesModule());
+        mapper.registerModule(new Jdk8Module());
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+}
--- src/main/java/com/fernando84/employeeapi/controller/EmployeeController.java
+++ src/main/java/com/fernando84/employeeapi/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import com.fernando84.employeeapi.service.TitleService;
 
 @RestController
 @RequestMapping("/api/employees")
+// @CrossOrigin(origins = "http://localhost:5173")
 public class EmployeeController {
 
     private EmployeeService employeeService;
