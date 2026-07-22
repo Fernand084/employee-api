98310d0f973471b032b69c9243507f1251575ca4
Fernand084 <edgar.lopezr84@outlook.com>
2026-07-22 14:24:34 -0600

    Class JwtAuthFilter.java extends OncePerRequestFilter and executes ONCE per request, intercepting the 'Authorization' Header, validates token, if valid 'authenticates' the user for that particular request in the Security Context.

---
+++ src/main/java/com/fernando84/employeeapi/service/JwtAuthFilter.java
@@ -0,0 +1,61 @@
+package com.fernando84.employeeapi.service;
+
+import com.fernando84.employeeapi.service.UserDetailsServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+
+        // If there's no header or the header does not start with "Brearer "
+        // continues without authentication
+        // (SecurityConfig decides if the endpoint requires authentication)
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String jwt = authHeader.substring(7); // removes "Bearer "
+        final String username = jwtService.extractUsername(jwt);
+
+        // Only authenticates if there is a user AND there's no one authenticated
+        // on that context
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null, // no credentials needed, token is authenticated.
+                        userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
