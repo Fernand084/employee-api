e6c22e4eae0a6f87dabd36d5e2cccc338ddde7c1
Fernand084 <edgar.lopezr84@outlook.com>
2026-08-03 20:33:49 -0600

    Added POST mapping controller to create a new department.

--- src/main/java/com/fernando84/employeeapi/controller/DepartmentsController.java
+++ src/main/java/com/fernando84/employeeapi/controller/DepartmentsController.java
@@ -3,16 +3,23 @@ package com.fernando84.employeeapi.controller;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fernando84.employeeapi.DTO.DepartmentCreateRequest;
 import com.fernando84.employeeapi.DTO.DepartmentDTO;
 import com.fernando84.employeeapi.DTO.EmployeeDTO;
 import com.fernando84.employeeapi.DTO.ManagerDTO;
+import com.fernando84.employeeapi.model.Department;
+import com.fernando84.employeeapi.service.DepartmentCreateService;
 import com.fernando84.employeeapi.service.DepartmentEmployeeService;
 import com.fernando84.employeeapi.service.DepartmentManagerService;
 import com.fernando84.employeeapi.service.DepartmentService;
@@ -27,10 +34,14 @@ public class DepartmentsController {
 
     private DepartmentManagerService departmentManagerService;
 
-    public DepartmentsController(DepartmentService ds, DepartmentEmployeeService des, DepartmentManagerService dms) {
+    private DepartmentCreateService departmentCreateService;
+
+    public DepartmentsController(DepartmentService ds, DepartmentEmployeeService des, DepartmentManagerService dms,
+            DepartmentCreateService dcs) {
         this.departmentService = ds;
         this.departmentEmployeeService = des;
         this.departmentManagerService = dms;
+        this.departmentCreateService = dcs;
     }
 
     @GetMapping
@@ -57,5 +68,12 @@ public class DepartmentsController {
         PageRequest pageable = PageRequest.of(page, size);
         Page<ManagerDTO> managers = departmentManagerService.findManagersByDepartmentId(pageable, id);
         return ResponseEntity.ok(managers);
+    }
+
+    @PostMapping()
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Department> create(@RequestBody DepartmentCreateRequest request) {
+        Department created = departmentCreateService.createDepartment(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 }
